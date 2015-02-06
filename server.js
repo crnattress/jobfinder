0dc7bea53f5c90e7db9bc1983695c411aa607cc1
Chris Nattress <cnattress@gmail.com>
2015-02-06 01:41:51 +0000

    tests for jobs get and refactored

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
-var mongoose = require('mongoose');
 var jobModel = require('./models/Job');
-
+var jobsData = require('./jobs-data.js');
 var app = express();
 
 app.set('views', __dirname);
@@ -10,23 +9,19 @@ app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/api/jobs', function (req, res) {
-   mongoose.model('Job').find({}).exec(function (error, collection) {
-      res.send(collection); 
-   });
+    jobsData.findJobs().then(function(collection) {
+       res.send(collection); 
+    });
 });
 
 app.get('*', function(req, res) {
    res.render('index'); 
 });
 
-// mongoose.connect('mongodb://localhost/jobfinder');
-mongoose.connect('mongodb://appuser:Password1@ds039421.mongolab.com:39421/jobfinder-01');
-
-var con = mongoose.connection;
-
-con.on('open', function () {
-    console.log('connected to mongodb successfully');
-    jobModel.seedJobs();
- });
+jobsData.connectDB('mongodb://appuser:Password1@ds039421.mongolab.com:39421/jobfinder-01')
+        .then(function() {
+           console.log('connected to mongodb successfully');
+           jobsData.seedJobs();
+        });
 
 app.listen(process.env.PORT, process.env.IP);
